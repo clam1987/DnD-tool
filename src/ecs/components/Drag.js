5464bc1f92dc12513d1b1e35230c7ffa55b00d2c
Wilson Lam <48064335+clam1987@users.noreply.github.com>
2025-04-12 00:23:20 -0700

    added troika and a Drag component

---
+++ src/ecs/components/Drag.js
@@ -0,0 +1,25 @@
+import { Component } from "geotic";
+
+export class Drag extends Component {
+  constructor() {
+    super();
+
+    this.start = { x: 0, y: 0, z: 0 };
+    this.current = { x: 0, y: 0, z: 0 };
+    this.active = false;
+  }
+
+  onAttached() {
+    this.start.x = this.entity.position.x;
+    this.start.y = this.entity.position.y;
+    this.start.z = this.entity.position.z;
+  }
+
+  onDragStart() {
+    this.active = true;
+  }
+
+  onDragEnd() {
+    this.active = false;
+  }
+}
